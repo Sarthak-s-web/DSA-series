bfe79de5c9f72fc05b66881fbac35fb4d263d636
Sarthak Garg <gargs0046@gmail.com>
2026-09-06 23:54:07 +0530

    Time: 0 ms (100%), Space: 9.7 MB (75.32%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int freq1[26]={0};
+        int freq2[26]={0};
+        int n=s.length();
+        int m=t.length();
+        if(n!=m)
+        {
+            return false;
+        }
+        else
+        {
+         for(int i=0;i<n;i++)
+        {
+            freq1[s[i]-'a']++;
+        }
+         for(int i=0;i<m;i++)
+        {
+            freq2[t[i]-'a']++;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(freq1[i]!=freq2[i])
+            {
+                return false;
+            }
+        }
+        }
+        return true;
+    }
+};
