053c0ec84b8142a654a61fec5077833313be175c
Sarthak Garg <gargs0046@gmail.com>
2026-09-23 13:47:32 +0530

    Time: 0 ms (100%), Space: 8.7 MB (35.96%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0) return false;
+        long long temp=x;
+        long long rev=0;
+        while(temp>0)
+        {
+            int digit=temp%10;
+            rev=rev*10+digit;
+            temp=temp/10;
+        }
+
+        if(x==rev) return true;
+        
+        return false;
+    }
+};
